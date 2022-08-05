b793e90531dcf31d3f2c81022fbefd964345e6cb
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-05 11:19:39 -0400

    :sparkles: [ADD] PlatziPunks contract
    - Add contract
    - Add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+const deploy=async ()=>{
+    const [deployer] = await ethers.getSigners(); //get info about our private key
+    console.log("Deploying contracts with the account:",deployer.address); //print info about deployer address
+    console.log("Account balance:",(await deployer.getBalance()).toString()); //get account balance and print
+   
+    const Token=await ethers.getContractFactory("PlatziPunks"); // get the contract
+    const token=await Token.deploy(); //deploy the contract
+    
+    console.log("Token address:",token.address); //print the contract address
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+deploy()
+  .then(() => process.exit(0)) // show process success
+  .catch((error) => { //if not show the error and launch error process
+    console.log(error);
+    process.exit(1);
+  });
