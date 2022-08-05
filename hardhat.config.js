5d580048102cb65cb847b483d03ea32cb9fdf8d7
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-05 10:05:22 -0400

    :rocket: [ADD] .env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+const {DEPLOY_SIGNER_PRIVATE_KEY, INFURA_PROJECT_ID}= process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks:{   
+    ropsten:{
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [
+        DEPLOY_SIGNER_PRIVATE_KEY
+      ]
+    }
+  }
 };
