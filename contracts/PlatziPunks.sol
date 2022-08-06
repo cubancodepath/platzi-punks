c4e99f9847a3e575931c04f7a5ee4a501b6cb4c8
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-06 11:27:35 -0400

    :sparkles: [ADD] image url

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -12,17 +12,66 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA{
     using Counters for Counters.Counter;
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
+    string URI;
+    mapping (uint256=>uint256) tokenDNA;
 
-    constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLTPKS"){
+    constructor(uint256 _maxSupply, string memory _URI) ERC721("PlatziPunks", "PLTPKS"){
         maxSupply=_maxSupply;
+        URI=_URI;
     }
 
     function mint() public{
         uint256 tokenId=_idCounter.current();
         require(tokenId<maxSupply, 'Not PlatziPunks available');
+
+        tokenDNA[tokenId]=deterministicPseudoRandomDNA(tokenId, msg.sender);
         _safeMint(msg.sender, tokenId);
         _idCounter.increment();
     }
+    function _paramsURI(uint256 _dna) internal view returns(string memory){
+        string memory params;
+
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+         return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
+    }
+
 
     function tokenURI(uint256 tokenId)
         public 
@@ -32,12 +81,15 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA{
             require(_exists(tokenId), 
             'ERC721 Metadata: URI form not existing token');
 
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+
         string memory jsonURI=Base64.encode(
             abi.encodePacked(
                 '{ "name": "PlatziPunks #',
                 tokenId,
                 '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image":"',
-                "//TODO Calculate image url",
+                image,
                 '"}'
         ));
 
@@ -59,5 +111,13 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA{
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    } 
+    }
+
+    function _getURI() internal view returns(string memory){
+        return URI;
+    }
+
+    function _setURI(string memory _URI) internal {
+        URI=_URI;
+    }
 }
--- contracts/PlatziPunksDNA.sol
+++ contracts/PlatziPunksDNA.sol
@@ -201,7 +201,7 @@ contract PlatziPunksDNA{
         "ShortHairTheCaesarSidePart"
     ];
 
-        // This pseudo random function is determistic and should not be used on production
+        // This pseudo random function is deterministic and should not be used on production
     function deterministicPseudoRandomDNA(uint256 _tokenId, address _minter)
         public
         pure
@@ -229,7 +229,7 @@ contract PlatziPunksDNA{
             );
     }
 
-    function _getAccesoriesType(uint8 _dna)
+    function getAccessoriesType(uint256 _dna)
         public
         view
         returns (string memory)
@@ -238,7 +238,7 @@ contract PlatziPunksDNA{
         return _accessoriesType[dnaSection % _accessoriesType.length];
     }
 
-    function _getClotheColor(uint8 _dna) public view returns (string memory) {
+    function getClotheColor(uint256 _dna) public view returns (string memory) {
         uint8 dnaSection = _getDNASection(_dna, 2);
         return _clotheColor[dnaSection % _clotheColor.length];
     }
